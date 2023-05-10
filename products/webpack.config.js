09ba70973c5caf769f0edd3b30851997eefd70df
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-10 16:34:50 +0530

    Configure asynchronous loading for each car and products app so that they can load as standalone.

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
       exposes: {
         "./ProductsIndex": "./src/index",
       },
+      shared: ["faker"],
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
